edae9c01f90291673cfd5eb4441123d5425a4f56
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-08-11 16:12:03 +0200

    Use AppCompatResources for vectors

--- notes/notes-impl/src/main/kotlin/com/fondesa/notes/notes/impl/NoteFloatingActionButton.kt
+++ notes/notes-impl/src/main/kotlin/com/fondesa/notes/notes/impl/NoteFloatingActionButton.kt
@@ -3,7 +3,7 @@ package com.fondesa.notes.notes.impl
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.ContextCompat
+import androidx.appcompat.content.res.AppCompatResources
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NoteFloatingActionButton @JvmOverloads constructor(
@@ -27,9 +27,15 @@ class NoteFloatingActionButton @JvmOverloads constructor(
             field = value
         }
 
-    private val drawableAdd by lazy { ContextCompat.getDrawable(context, R.drawable.ic_add) }
-    private val drawableCancel by lazy { ContextCompat.getDrawable(context, R.drawable.ic_cancel) }
-    private val drawableDone by lazy { ContextCompat.getDrawable(context, R.drawable.ic_done) }
+    private val drawableAdd by lazy {
+        AppCompatResources.getDrawable(context, R.drawable.ic_add)
+    }
+    private val drawableCancel by lazy {
+        AppCompatResources.getDrawable(context, R.drawable.ic_cancel)
+    }
+    private val drawableDone by lazy {
+        AppCompatResources.getDrawable(context, R.drawable.ic_done)
+    }
     private val listeners = mutableMapOf<NoteButtonState, () -> Unit>()
 
     init {
