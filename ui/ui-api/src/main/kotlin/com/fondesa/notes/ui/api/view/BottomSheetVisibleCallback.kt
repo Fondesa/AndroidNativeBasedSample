92772aa0ec86452c096f46bccd5f13f5f9efd936
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-08-14 22:41:05 +0200

    Fix NaN slide offset

--- ui/ui-api/src/main/kotlin/com/fondesa/notes/ui/api/view/BottomSheetVisibleCallback.kt
+++ ui/ui-api/src/main/kotlin/com/fondesa/notes/ui/api/view/BottomSheetVisibleCallback.kt
@@ -10,6 +10,12 @@ class BottomSheetVisibleCallback(private val listener: Listener) :
     private var sheetPreviouslyHidden = true
 
     override fun onSlide(view: View, slideOffset: Float) {
+        if (slideOffset.isNaN()) {
+            // On certain APIs the slide offset can be NaN when it reached the maximum height.
+            // In this case, it shouldn't be computed since this callback will be invoked with NaN only
+            // if previously it has been invoked with 1.0f (apparently).
+            return
+        }
         val isHidden = slideOffset == -1f
         if (isHidden && !sheetPreviouslyHidden) {
             listener.onBottomSheetHidden()
